66bb54acff551c08571970a3bce6fc589635631b
LanderDK <landerdekesel@gmail.com>
2023-10-30 22:50:51 +0100

    Initial Commit

---
+++ blitzware-ios/blitzware_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  blitzware_iosApp.swift
+//  blitzware-ios
+//
+//  Created by Lander De Kesel on 30/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct blitzware_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

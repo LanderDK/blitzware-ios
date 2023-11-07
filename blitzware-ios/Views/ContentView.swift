c263627f086364f5243ad4e653f11dfd7618267b
LanderDK <landerdekesel@gmail.com>
2023-11-07 11:16:54 +0100

    bottom navbar when authed

--- blitzware-ios/Views/ContentView.swift
+++ blitzware-ios/Views/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
     
     var body: some View {
         if viewModel.isAuthed {
-            AuthenticatedView()
+            BottomNavBar()
         } else {
             LoginView()
         }
@@ -67,7 +67,7 @@ struct LoginView: View {
     }
 }
 
-struct AuthenticatedView: View {
+struct ApplicationsList: View {
     @EnvironmentObject var viewModel: AppViewModel
     @State private var isShowingAlert = false
     @State private var alertTitle = ""
@@ -75,87 +75,120 @@ struct AuthenticatedView: View {
     @State private var isShowingAddSheet = false
     
     var body: some View {
-        HStack {
-            Text("Welcome back, \(viewModel.accountData!.account.username)!")
+        VStack {
+            HStack {
+                Text("Welcome back, \(viewModel.accountData!.account.username)!")
+                    .font(.title)
+                    .padding()
+                Spacer()
+                Button {
+                    isShowingAddSheet = true
+                } label: {
+                    Image(systemName: "plus.app.fill")
+                }
                 .font(.title)
                 .padding()
-            Spacer()
-            Button {
-                isShowingAddSheet = true
-            } label: {
-                Image(systemName: "plus")
+                .accentColor(Color(red: 25/255, green: 118/255, blue: 210/255))
             }
-            .font(.title)
-            .padding()
-        }
-        VStack {
-            if viewModel.requestState == .error {
-                Text(viewModel.errorData?.message ?? "Unkown error")
-                    .foregroundColor(.red)
-            }
-            if viewModel.requestState == .pending || viewModel.requestState == .sent {
-                ProgressView()
-            } else {
-                List(viewModel.applications, id: \.id) { application in
-                    ApplicationRowView(application: application)
-                        .contextMenu {
-                            CustomButton(title: "App-Panel") {
-                                Task {
-                                    await viewModel.login(username: "username", password: "password")
-                                }
-                            }
-                            if application.status == 1 {
-                                CustomButton(title: "Disable") {
-                                    let newApp = ApplicationData(id: application.id, name: application.name, secret: application.secret, status: 0, hwidCheck: application.hwidCheck, developerMode: application.developerMode, integrityCheck: application.integrityCheck, freeMode: application.freeMode, twoFactorAuth: application.twoFactorAuth, programHash: application.programHash, version: application.version, downloadLink: application.downloadLink, adminRoleId: application.adminRoleId, adminRoleLevel: application.adminRoleLevel)
-                                    Task {
-                                        await viewModel.updateApplicationById(application: newApp)
-                                    }
-                                    if let index = viewModel.applications.firstIndex(where: { $0.id == application.id }) {
-                                        viewModel.applications[index].status = 0
-                                    }
-                                }
-                            } else {
-                                CustomButton(title: "Enable") {
-                                    let newApp = ApplicationData(id: application.id, name: application.name, secret: application.secret, status: 1, hwidCheck: application.hwidCheck, developerMode: application.developerMode, integrityCheck: application.integrityCheck, freeMode: application.freeMode, twoFactorAuth: application.twoFactorAuth, programHash: application.programHash, version: application.version, downloadLink: application.downloadLink, adminRoleId: application.adminRoleId, adminRoleLevel: application.adminRoleLevel)
-                                    Task {
-                                        await viewModel.updateApplicationById(application: newApp)
-                                    }
-                                    if let index = viewModel.applications.firstIndex(where: { $0.id == application.id }) {
-                                        viewModel.applications[index].status = 1
-                                    }
-                                }
-                            }
-                            CustomButton(title: "Delete") {
-                                Task {
-                                    await viewModel.deleteApplicationById(applicationId: application.id)
-                                    if viewModel.requestState == .success {
-                                        alertTitle = "Success!"
-                                        alertMessage = "Application was deleted successfully."
-                                        isShowingAlert = true
-                                    } else {
-                                        alertTitle = "Oops!"
-                                        alertMessage = viewModel.errorData?.message ?? "Unkown error"
-                                        isShowingAlert = true
-                                    }
-                                }
-                            }
-                        }
+            VStack {
+                if viewModel.requestState == .error {
+                    Text(viewModel.errorData?.message ?? "Unkown error")
+                        .foregroundColor(.red)
                 }
+                if viewModel.requestState == .pending || viewModel.requestState == .sent {
+                    ProgressView()
+                } else {
+                    List(viewModel.applications, id: \.id) { application in
+                        ApplicationRowView(application: application)
+                            .contextMenu {
+                                CustomButton(title: "Panel") {
+                                    Task {
+                                        await viewModel.login(username: "username", password: "password")
+                                    }
+                                }
+                                if application.status == 1 {
+                                    CustomButton(title: "Disable") {
+                                        let newApp = ApplicationData(id: application.id, name: application.name, secret: application.secret, status: 0, hwidCheck: application.hwidCheck, developerMode: application.developerMode, integrityCheck: application.integrityCheck, freeMode: application.freeMode, twoFactorAuth: application.twoFactorAuth, programHash: application.programHash, version: application.version, downloadLink: application.downloadLink, adminRoleId: application.adminRoleId, adminRoleLevel: application.adminRoleLevel)
+                                        Task {
+                                            await viewModel.updateApplicationById(application: newApp)
+                                        }
+                                        if let index = viewModel.applications.firstIndex(where: { $0.id == application.id }) {
+                                            viewModel.applications[index].status = 0
+                                        }
+                                    }
+                                } else {
+                                    CustomButton(title: "Enable") {
+                                        let newApp = ApplicationData(id: application.id, name: application.name, secret: application.secret, status: 1, hwidCheck: application.hwidCheck, developerMode: application.developerMode, integrityCheck: application.integrityCheck, freeMode: application.freeMode, twoFactorAuth: application.twoFactorAuth, programHash: application.programHash, version: application.version, downloadLink: application.downloadLink, adminRoleId: application.adminRoleId, adminRoleLevel: application.adminRoleLevel)
+                                        Task {
+                                            await viewModel.updateApplicationById(application: newApp)
+                                        }
+                                        if let index = viewModel.applications.firstIndex(where: { $0.id == application.id }) {
+                                            viewModel.applications[index].status = 1
+                                        }
+                                    }
+                                }
+                                CustomButton(title: "Delete") {
+                                    Task {
+                                        await viewModel.deleteApplicationById(applicationId: application.id)
+                                        if viewModel.requestState == .success {
+                                            alertTitle = "Success!"
+                                            alertMessage = "Application was deleted successfully."
+                                            isShowingAlert = true
+                                        } else {
+                                            alertTitle = "Oops!"
+                                            alertMessage = viewModel.errorData?.message ?? "Unkown error"
+                                            isShowingAlert = true
+                                        }
+                                    }
+                                }
+                            }
+                    }
+                }
+            }.onAppear(perform: {
+                Task {
+                    await viewModel.getApplicationsOfAccount()
+                }
+            })
+            .alert(isPresented: $isShowingAlert) {
+                Alert(
+                    title: Text(alertTitle),
+                    message: Text(alertMessage),
+                    dismissButton: .default(Text("OK"))
+                )
             }
-        }.onAppear(perform: {
-            Task {
-                await viewModel.getApplicationsOfAccount()
+            .sheet(isPresented: $isShowingAddSheet) {
+                AddApplicationView(isPresented: $isShowingAddSheet)
             }
-        })
-        .alert(isPresented: $isShowingAlert) {
-            Alert(
-                title: Text(alertTitle),
-                message: Text(alertMessage),
-                dismissButton: .default(Text("OK"))
-            )
         }
-        .sheet(isPresented: $isShowingAddSheet) {
-            AddApplicationView(isPresented: $isShowingAddSheet)
+    }
+}
+
+struct BottomNavBar: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                ApplicationsList()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Apps")
+                    }
+                Text("Resources")
+                    .tabItem {
+                        Image(systemName: "book.fill")
+                        Text("Resources")
+                    }
+                Text("Community")
+                    .tabItem {
+                        Image(systemName: "person.3.fill")
+                        Text("Community")
+                    }
+                Text("Account")
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Account")
+                    }
+            }
+            .accentColor(Color(red: 253/255, green: 126/255, blue: 20/255))
         }
     }
 }
@@ -164,27 +197,38 @@ struct AddApplicationView: View {
     @EnvironmentObject var viewModel: AppViewModel
     @Binding var isPresented: Bool
     @State private var applicationName = ""
+    @FocusState private var isTextFieldFocused: Bool
 
     var body: some View {
         NavigationView {
             Form {
                 TextField("Application name", text: $applicationName)
+                    .focused($isTextFieldFocused)
             }
-            .navigationBarItems(
-                trailing: Button("Create") {
-                    if !applicationName.isEmpty {
-                        Task {
-                            await viewModel.createApplication(name: applicationName)
-                        }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
                         isPresented = false
                     }
                 }
-            )
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Create") {
+                        if !applicationName.isEmpty {
+                            Task {
+                                await viewModel.createApplication(name: applicationName)
+                            }
+                            isPresented = false
+                        }
+                    }
+                }
+            }
             .navigationBarTitle("Create an application", displayMode: .inline)
+        }
+        .onAppear {
+            isTextFieldFocused = true
         }
     }
 }
-
 
 struct ApplicationRowView: View {
     var application: ApplicationData
